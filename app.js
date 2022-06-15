7e0348d6b61bd142cf0c92668c1cf4960f6bf08e
jcvellez <jcvellez@live.com>
2022-06-14 22:41:42 -0500

    Me toca la clase 11

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
-let nombre = prompt('Cual es tu nombre');
-let edad = prompt('Cual es tu edad');
+// let nombre = prompt('Cual es tu nombre');
+// let edad = prompt('Cual es tu edad');
+// document.getElementById('app').innerHTML=`Bienvenido ${nombre} de ${edad} años`;
+// console.log('Enviado a cnsola');
 
-document.getElementById('app').innerHTML=`Bienvenido ${nombre} de ${edad} años`;
-console.log('Enviado a cnsola');
+var nombre='Juan';
+var nombre='Miguel';
+
+console.log(nombre);
